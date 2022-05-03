8814ee6a0c777b72e3edee1903c5cafb92973af6
Crowdbotics <team@crowdbotics.com>
2022-05-03 03:33:54 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/erscom_35049/MainActivity.java
@@ -0,0 +1,15 @@
+package com.erscom_35049;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "erscom_35049";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "erscom_35049",
+  url: "https://erscom_35049.botics.co",
+  api: "https://erscom_35049.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
